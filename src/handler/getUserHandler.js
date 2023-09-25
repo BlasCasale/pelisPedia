772addb666dbac71c9168cc216bfd5e132b67053
BlasCasale <casale.blas@live.com>
2023-09-25 20:47:49 -0300

    pequeños cambios en las regExp y el handler

--- src/handler/getUserHandler.js
+++ src/handler/getUserHandler.js
@@ -6,7 +6,7 @@ const getUserHandler = async (req, res) => {
         const user = await getUser({ mail, password });
         res.status(200).json(user);
     } catch (error) {
-        res.status(404).json({ error: error.message });
+        res.status(500).send({ error: error });
     };
 };
 
