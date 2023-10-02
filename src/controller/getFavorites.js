7d1b60affe74faa858904fb8d6cab0a5f7186117
BlasCasale <casale.blas@live.com>
2023-10-02 15:38:29 -0300

    correccion en los favorites

--- src/controller/getFavorites.js
+++ src/controller/getFavorites.js
@@ -1,9 +1,21 @@
 const { Favorite } = require('../db');
+const axios = require('axios');
+const { API_KEY } = process.env;
 
 const getFavorites = async (UserId) => {
+    const response = [];
     const userFavorites = await Favorite.findAll({ where: { UserId } });
-    if(!userFavorites) throw Error('No hay peliculas con me gusta en nuestra base de datos.')
-    return userFavorites;
+    if (!userFavorites) throw Error('No hay peliculas con me gusta en nuestra base de datos.');
+
+    const promises = userFavorites.map((movie) => {
+        return axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${movie.imdbID}`)
+    });
+
+    Promise.all(promises)
+        .then((responses) => {
+            responses.forEach((res) => response.push(res.data));
+        });
+    return response;
 };
 
 module.exports = getFavorites;
